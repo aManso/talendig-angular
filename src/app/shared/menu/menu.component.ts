2e3dafc36ce850b8c23c50967d3732154b6eae17
Alejandro Manso <alejandro.mansogonzalez@kbc.be>
2022-06-23 15:53:04 +0200

    arreglar errores para visualizar clase de componentes

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,8 +1,5 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { NgModel } from '@angular/forms';
-import { Capital, Pais } from 'src/app/paises/paises';
-import { LoggerService } from '../servicios/logger.service';
-import { PaisesService } from '../servicios/paises.service';
 
 @Component({
   selector: 'app-menu',
@@ -12,15 +9,11 @@ import { PaisesService } from '../servicios/paises.service';
 export class MenuComponent implements OnInit, AfterViewInit{
   public edad = 30;
   @Input('colorMenu') color:string = 'red';
-  @Output() alerta: EventEmitter<Capital> = new EventEmitter<Capital>()
   @ViewChild(NgModel) inputColor: NgModel;
 
 
   public constructor(
-    private _paisesService: PaisesService,
-    private _loggerService: LoggerService,
   ) {
-    this._loggerService.log('MenuComponent');
   }
 
   public ngOnInit(): void {
@@ -29,36 +22,6 @@ export class MenuComponent implements OnInit, AfterViewInit{
 
   public ngAfterViewInit(): void {
     console.log(this.inputColor);
-  }
-
-  // public cambiarFondo(): void {
-  //   this.cambiarFondoEmitter.emit(true);
-  // }
-
-  public mostrarAlerta() {
-    console.log(this.inputColor);
-    const miCapital: Capital = {
-      nombre: 'Paris',
-      poblacion: 3122312,
-      fecha: new Date()
-    }
-    this.alerta.emit(miCapital);
-  }
-
-  public mostrarPais() {
-    // Obtenemos la lista actual de paises
-    const paises = this._paisesService.getListaDePaises();
-    // Anadimos un nuevo pais
-    paises.push({
-      nombre: 'Japon',
-      poblacion: 231,
-      capital: {
-        nombre: 'tokio',
-        poblacion: 2131
-      }
-    } as Pais)
-    // guardamos todos los paises de nuevo en el servicio
-    this._paisesService.setListaDePaises(paises);
   }
 
 }
